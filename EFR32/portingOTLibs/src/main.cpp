a334792de75353793a92aa6ec7c048adabfd1538
Deniz Erbilgin <denzo@localhost.localdomain>
2017-12-14 17:28:33 +0000

    porting work

--- EFR32/portingOTLibs/src/main.cpp
+++ EFR32/portingOTLibs/src/main.cpp
@@ -12,7 +12,6 @@ extern "C" {
 
 #define DEBUG_BREAK __asm__("BKPT #0");
 
-
 extern "C" {
 // SysTick emulates subcycleTime
 // auto subCycleTime = 0U;
@@ -25,19 +24,28 @@ void SysTick_Handler(void) {
 
 int main(void)
 {
+    constexpr auto F_CPU = 38000000U;
     /* Chip errata */
     CHIP_Init();
 
     if(OTV0P2BASE::setupEmulated2sSubCycle()) { DEBUG_BREAK; }
-
+    
     setupGPIO();
-    auto ledState = false;
+    auto led0State = true;
+    auto led1State = true;
     /* Infinite loop */
     while (1) {
-        if(255U == OTV0P2BASE::getSubCycleTime()) {
-          ledState = !ledState;
-        //   setLED<gpioPortF, 4>(ledState);
-          setLED<gpioPortF, 5>(!ledState);
+        // Flip LED every time we loop. Should take ~2 s
+        led0State = !led0State;
+        setLED<gpioPortF, 4>(led0State);
+        for(auto i = 0; i < 2000; ++i) {
+            // Poll subCycleTime and delay 1 ms
+            if(255U == OTV0P2BASE::getSubCycleTime()) {
+                led1State = !led1State;
+                setLED<gpioPortF, 5>(!led1State);
+                while(255U == OTV0P2BASE::getSubCycleTime()) {}
+            }
+            OTV0P2BASE::delay_ms(1);
         }
     }
 }
